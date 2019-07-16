018b60ebb934c3212959a25f5ccc3d8b6f82b91f
Pete Bryan <pebryan@microsoft.com>
2019-07-16 14:34:40 -0700

    removed un-needed functions

--- msticpy/sectools/linuxsyslog.py
+++ msticpy/sectools/linuxsyslog.py
@@ -18,12 +18,11 @@ get_user_mods_syslog - collects and returns data on all user and group additions
 get_syslog_events - collects and retunrs all syslog messages for the host being investigated along with a list of the facilities present
 syslog_volume_graph - displays a graph of syslog message volume over time
 """
-import collections
 import datetime as dt
 import os
 import re
 from datetime import datetime
-from typing import Any, Dict, List, Mapping, Optional, Set, Tuple
+from typing import Any, Dict, List, Optional, Set, Tuple
 
 import ipywidgets as widgets
 import matplotlib.pyplot as plt
@@ -58,6 +57,11 @@ register_matplotlib_converters()
 
 class Error(Exception):
     """Base class for other exceptions"""
+
+    pass
+
+class DataError(Error):
+    """Raised when thereis a data input error"""
 
     pass
 
@@ -104,52 +108,8 @@ def convert_to_ip_entities(ip_str: str) -> Tuple[IpAddress]:
             ip_entities.append(ip_entity)
     return ip_entities
 
-
-@export
-def syslog_host_picker(time: int = 90) -> List[str]:
-    """
-    Returns a list of Computer names that have generated Syslog messages within the last 90 days
-
-    Parameters
-    ----------
-    time: int
-        The number of preceding days to check for hosts with syslog data in, default is 90
-
-    Returns
-    ----------
-    items: List
-        A list of Computer names
-
-    Raises
-    ----------
-    KQLDataError
-        There are no hosts which have generated syslog messages within the last the last 90 days.
-
-    """
-    syslog_hosts_query = f""" Syslog 
-    | where TimeGenerated > ago({time}d) 
-    | summarize SyslogCount=count(SyslogMessage) by Computer"""
-    kql_raw_results = _ip.magic("kql -query syslog_hosts_query")
-    if kql_raw_results.completion_query_info["StatusCode"] == 0:
-        syslog_hosts = kql_raw_results.to_dataframe()
-
-    if syslog_hosts.empty:
-        raise KQLDataError(
-            "There are no hosts which have generated syslog messages within the last the last 90 days."
-        )
-
-    else:
-        display(
-            Markdown(f"### Hosts With Syslog Generated Within the Previous {time} Days")
-        )
-        items = syslog_hosts["Computer"].values.tolist()
-
-    return items
-
-
 @export
 def create_host_record(syslog_df: pd.DataFrame, heartbeat_df: pd.DataFrame, az_net_df: pd.DataFrame = None) -> Host:
-    # remove queries from function and push to notebook.
     """
     Generate host_entity record for selected computer
 
@@ -183,7 +143,7 @@ def create_host_record(syslog_df: pd.DataFrame, heartbeat_df: pd.DataFrame, az_n
         applications = []
         _apps = syslog_df['ProcessName'].unique().tolist()
         for app in _apps:
-            if app not in ("CRON", "systemd-resolved", "systemd", "crontab", "systemd-timesyncd", "systemd-logind"):
+            if app not in ("CRON", "sudo", "snapd" "systemd-resolved", "systemd", "crontab", "systemd-timesyncd", "systemd-logind", "rsyslogd", "syslog-ng"):
                 applications.append(app)
             else:
                 pass
@@ -288,11 +248,6 @@ def cluster_syslog_logons(logon_events: pd.DataFrame) -> dict:
                 user = (logons_opened.iloc[ses_opened]).Sudoer
             else:
                 user = "Unknown"
-            if isinstance(ses_start, datetime) == False:
-                ses_start = datetime.strptime(ses_start,"%Y-%m-%d %H:%M:%S")
-                ses_end = datetime.strptime(ses_end,"%Y-%m-%d %H:%M:%S")
-            else:
-                pass
             if ses_start > ses_close_time or ses_opened == 0:
                 pass
             else:
@@ -314,266 +269,28 @@ def cluster_syslog_logons(logon_events: pd.DataFrame) -> dict:
 
 
 @export
-def get_cron_activity_syslog(
-    hostname: str, start: datetime, end: datetime
-) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-    """
-    Collects and returns data on syslog messages related to cron activity on the computer defined in the time window specified
-    Parameters
-    ----------
-    hostname : str
-        The Computer name of the computer being investigated
-    start : datetime
-        The start of the time to query data regarding the computer for
-    end : datetime
-        The end of the time to query data regarding the computer for
-
-    Returns
-    ----------
-    cron_act: namedTuple
-        Contains:
-            all_cron_events - Panda DataFrame of all syslog messages related to cron activity
-            cron_edits - Panda DataFrame of all syslog messages realted to cron file edits
-            cron_executions - Panda DataFrame of all syslog messages related to cron executions
-       
-    Raises
-    ----------
-    KQLDataError
-        No cron activity found
-    """
-    cron_activity_query = f"""Syslog
-    | where TimeGenerated >= datetime({start})
-    | where TimeGenerated <= datetime({end})
-    | extend StartTimeUtc = TimeGenerated
-    | where Computer == "{hostname}"
-    | where ProcessName == "CRON" or Facility == "cron"
-    | extend CMD=extract("CMD(.*)",1,SyslogMessage), User=extract("for user ([[:alpha:]]*)",1,SyslogMessage), CronUser=extract("^[(]([[:alpha:]]*)",1,SyslogMessage),EditStatus=extract("[A-Z]+ EDIT",0,SyslogMessage)
-    """
-
-    kql_raw_result = _ip.magic("kql -query cron_activity_query")
-
-    if kql_raw_result.completion_query_info["StatusCode"] == 0:
-        all_cron_events = kql_raw_result.to_dataframe()
-    if all_cron_events.empty:
-        raise KQLDataError(f"No cron activity for {hostname} between {start} and {end}")
-    else:
-        all_cron_events.loc[all_cron_events["User"] == "", "User"] = np.nan
-        all_cron_events.loc[all_cron_events["CronUser"] == "", "CronUser"] = np.nan
-        all_cron_events.loc[all_cron_events["CMD"] == "", "CMD"] = np.nan
-        cron_edits = all_cron_events[
-            all_cron_events["SyslogMessage"].str.contains("EDIT")
-        ]
-        cron_executions = all_cron_events[["TimeGenerated", "CMD", "CronUser"]].dropna()
-        cron_act = collections.namedtuple(
-            "cron_act", ["all_cron_events", "cron_edits", "cron_executions"]
-        )
-        return cron_act(all_cron_events, cron_edits, cron_executions)
-
-
-@export
-def get_user_mods_syslog(
-    hostname: str, start: datetime, end: datetime
-) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, List, List, List]:
-    """
-    Collects and returns data on all user and group creations, deletions and modifications on the computer defined, in the time window specified
-
-    Parameters
-    ----------
-    hostname : str
-        The Computer name of the computer being investigated
-    start : datetime
-        The start of the time to query data regarding the computer for
-    end : datetime
-        The end of the time to query data regarding the computer for
-
-    Returns
-    ----------
-    user_mods: namedTuple
-        Contains:
-         add_events - Panda DataFrame of all syslog messages related to user or group additions
-         del_events - Panda DataFrame of all syslog messages related to user or group deletions
-         mod_events - Panda DataFrame of all syslog messages related to user or group modifications
-         add_users - List of user or group names added
-         del_users - List of user or group names deleted
-         mod_users - List of user or group names modified   
-
-    Raises
-    ----------
-    KQLDataError
-        No user or group modifications found
-    """
-    add_query = f"""Syslog
-    | where TimeGenerated >= datetime({start})
-    | where TimeGenerated <= datetime({end})
-    | extend StartTimeUtc = TimeGenerated
-    | where Computer == "{hostname}"
-    | where Facility == "authpriv"
-    | where SyslogMessage contains "new"
-    | extend User=extract("user: name=([[:alnum:]]+)",1,SyslogMessage), Group=extract("group: name=([[:alnum:]]+)",1,SyslogMessage), UID=extract("UID=([0-9]+)",1,SyslogMessage), GID=extract("GID=([0-9]+)",1,SyslogMessage)
-    """
-
-    del_query = f"""Syslog
-    | where TimeGenerated >= datetime({start})
-    | where TimeGenerated <= datetime({end})
-    | extend StartTimeUtc = TimeGenerated
-    | where Computer == "{hostname}"
-    | where Facility == "authpriv"
-    | where (SyslogMessage contains "delete" or SyslogMessage contains "removed") and ProcessName == "userdel"
-    | extend User=extract("user '([[:alnum:]]+)",1,SyslogMessage),Group=extract("group '([[:alnum:]]+)",1,SyslogMessage)
-    """
-
-    mod_query = f"""Syslog
-    | where TimeGenerated >= datetime({start})
-    | where TimeGenerated <= datetime({end})
-    | extend StartTimeUtc = TimeGenerated
-    | where Computer == "{hostname}"
-    | where Facility == "authpriv"
-    | where ProcessName == "usermod"
-    | extend User=extract("user '([[:alnum:]]+)",1,SyslogMessage)
-    """
-
-    kql_raw_result = _ip.magic("kql -query add_query")
-
-    if kql_raw_result.completion_query_info["StatusCode"] == 0:
-        add_events = kql_raw_result.to_dataframe()
-
-    kql_raw_result = _ip.magic("kql -query del_query")
-
-    if kql_raw_result.completion_query_info["StatusCode"] == 0:
-        del_events = kql_raw_result.to_dataframe()
-
-    kql_raw_result = _ip.magic("kql -query mod_query")
-
-    if kql_raw_result.completion_query_info["StatusCode"] == 0:
-        mod_events = kql_raw_result.to_dataframe()
-
-    if add_events.empty and del_events.empty and mod_events.empty:
-        raise KQLDataError(f"No user or group modications on {hostname}")
-    else:
-        if add_events.empty:
-            add_users = []
-            pass
-        else:
-            add_events.loc[add_events["User"] == "", "User"] = np.nan
-            add_users = add_events["User"].dropna().unique()
-            add_users = ", ".join(add_users)
-        if del_events.empty:
-            del_users = []
-            pass
-
-        else:
-            del_events.loc[del_events["User"] == "", "User"] = np.nan
-            del_users = del_events["User"].dropna().unique()
-            del_users = ", ".join(del_users)
-        if mod_events.empty:
-            mod_users = []
-            pass
-        else:
-            mod_events.loc[mod_events["User"] == "", "User"] = np.nan
-            mod_users = del_events["User"].dropna().unique()
-            mod_users = ", ".join(mod_users)
-        user_mods = collections.namedtuple(
-            "user_mods",
-            [
-                "add_events",
-                "del_events",
-                "mod_events",
-                "add_users",
-                "del_users",
-                "mod_users",
-            ],
-        )
-        return user_mods(
-            add_events, del_events, mod_events, add_users, del_users, mod_users
-        )
-
-
-@export
-def get_syslog_events(
-    hostname: str, start: datetime, end: datetime
-) -> Tuple[pd.DataFrame, pd.DataFrame, List]:
-    """
-    Collects and returns all syslog events, a count of the number of events returned and the syslog facilities observed
-
-    Parameters
-    ----------
-    hostname : str
-        The Computer name of the computer being investigated
-    start : datetime
-        The start of the time to query data regarding the computer for
-    end : datetime
-        The end of the time to query data regarding the computer for
-
-    Returns
-    ----------
-    syslog_data: namedTuple
-        Contains:
-           all_syslog_events - Panda DataFrame containing all the syslog messages associated with the computer in the defined time window
-           syslog_volume - Panda DataFrame containing details of syslog volumes over times
-           syslog_facilities - List of syslog facilities present in the all_syslog_events dataset
-
-    Raises
-    ----------
-    KQLDataError
-        No syslog data found
-    """
-    all_syslog_query = f'''Syslog
-    | where TimeGenerated >= datetime({start})
-    | where TimeGenerated <= datetime({end})
-    | extend StartTimeUtc = TimeGenerated
-    | where Computer == "{hostname}"'''
-
-    syslog_volume_query = f"""Syslog
-    | where TimeGenerated >= datetime({start})
-    | where TimeGenerated <= datetime({end})
-    | extend StartTimeUtc = TimeGenerated
-    | where Computer == "{hostname}"
-    | summarize LogCount=count(SyslogMessage) by bin(TimeGenerated, 1h)"""
-
-    kql_raw_result = _ip.magic("kql -query all_syslog_query")
-
-    if kql_raw_result.completion_query_info["StatusCode"] == 0:
-        all_syslog_events = kql_raw_result.to_dataframe()
-
-    if all_syslog_events.empty:
-        raise KQLDataError("No syslog data found")
-    else:
-        kql_raw_result = _ip.magic("kql -query syslog_volume_query")
-
-        if kql_raw_result.completion_query_info["StatusCode"] == 0:
-            syslog_volume = kql_raw_result.to_dataframe()
-
-        syslog_facilities = all_syslog_events["Facility"].unique()
-        syslog_data = collections.namedtuple(
-            "syslog_data", ["all_syslog_events", "syslog_volume", "syslog_facilities"]
-        )
-        return syslog_data(all_syslog_events, syslog_volume, syslog_facilities)
-
-
-@export
 def sudo_risk_actions(sudo_events:pd.DataFrame, risky_stuff:str = os.path.join(os.path.join(os.path.dirname(__file__), _DETECTIONS_DEF_DIR), 'sudo_cmd_line.txt')):
     if 'Command' not in sudo_events.columns:
-        #make exception
-        print("DataFrame must contain column of commands run called 'Command'")
+        raise DataError("DataFrame must contain column of commands run called 'Command'")
     else:
         pass
-    risky_actions = []
+    risky_actions = {}
     sudo_events['Command'].replace('', np.nan, inplace=True)
-    sudo_actions = sudo_events['Command'].dropna().to_dict()
+    sudo_actions = sudo_events[['Command','TimeGenerated']].dropna().set_index('TimeGenerated').to_dict()
     risky_lines = [line.rstrip('\n') for line in open(risky_stuff)]
     for line in risky_lines:
-        for action in sudo_actions:
-            if re.match("(?P<b64>(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$)", sudo_actions[action]):
-                b64match = re.search("(?P<b64>(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$)", sudo_actions[action])
+        for date, command in sudo_actions['Command'].items():
+            if re.match("(?P<b64>(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$)", command):
+                b64match = re.search("(?P<b64>(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$)", command)
                 b64string = unpack(input_string=b64match[1])
                 b64string = b64string[1]['decoded_string'].to_string()
-                if re.match(line, sudo_actions[action]):
-                    risky_actions.append(sudo_actions[action])
+                if re.match(line, command):
+                    risky_actions.update({date:command})
                 else:
                     pass
             else:
-                if re.match(line, sudo_actions[action]):
-                    risky_actions.append(sudo_actions[action])
+                if re.match(line, command):
+                    risky_actions.update({date:command})
                 else:
                     pass
     return risky_actions
@@ -581,8 +298,7 @@ def sudo_risk_actions(sudo_events:pd.DataFrame, risky_stuff:str = os.path.join(o
 @export
 def sudo_actions_speed(sudo_events: pd.DataFrame, time: int = 5, events: int = 5):
     if 'Command' not in sudo_events.columns:
-        #make exception
-        print("DataFrame must contain column of commands run called 'Command'")
+        raise DataError("DataFrame must contain column of commands run called 'Command'")
     elif isinstance(sudo_events["TimeGenerated"][0], datetime) == False:
         print("TimeGenerated is not a datetime format")
     else:
