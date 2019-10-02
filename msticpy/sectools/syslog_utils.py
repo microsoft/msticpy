c4e541f0efd528e5e85e7f58ba40f748fb469ebb
Pete Bryan <pebryan@microsoft.com>
2019-10-02 16:05:43 -0700

    Seperated out cmd_line elements

--- msticpy/sectools/syslog_utils.py
+++ msticpy/sectools/syslog_utils.py
@@ -12,14 +12,10 @@ Designed to support standard linux syslog for investigations where auditd is not
 
 """
 import datetime as dt
-import os
-import re
-import json
 from typing import Tuple
 import pytz
 
 import ipywidgets as widgets
-import numpy as np
 import pandas as pd
 from pandas.plotting import register_matplotlib_converters
 from IPython import get_ipython
@@ -28,7 +24,6 @@ from .._version import VERSION
 from ..nbtools.entityschema import GeoLocation, Host, IpAddress
 from ..nbtools.utility import export
 from .geoip import GeoLiteLookup
-from .base64unpack import unpack
 
 __version__ = VERSION
 __author__ = "Pete Bryan"
@@ -166,91 +161,7 @@ def create_host_record(
 
 
 @export
-def cluster_syslog_logons(logon_events: pd.DataFrame) -> dict:
-    """
-    Clusters logon sessions observed in syslog by start and end time based on PAM events.
-
-    Parameters
-    ----------
-    logon_events: pd.DataFrame
-        A DataFrame of all syslog logon events (can be generated with LinuxSyslog.user_logon query)
-
-    Returns
-    -------
-    logon_sessions: dict
-        A dictionary of logon sessions including start and end times and logged on user
-
-    Raises
-    ------
-    KQLDataError
-        There are no logon sessions in the supplied data set
-
-    """
-    logon_sessions = {}
-    ses_close_time = dt.datetime.now()
-    ses_opened = 0
-    ses_closed = 0
-    # Extract logon session opened and logon session closed data.
-    logons_opened = (
-        (
-            logon_events.loc[
-                logon_events["SyslogMessage"].str.contains("pam_unix.+session opened")
-            ]
-        )
-        .set_index("TimeGenerated")
-        .sort_index(ascending=True)
-    )
-    logons_closed = (
-        (
-            logon_events.loc[
-                logon_events["SyslogMessage"].str.contains("pam_unix.+session closed")
-            ]
-        )
-        .set_index("TimeGenerated")
-        .sort_index(ascending=True)
-    )
-
-    if logons_opened.empty or logons_closed.empty:
-        raise DataError("There are no logon sessions in the supplied data set")
-
-    # For each session identify the likely start and end times
-    while ses_opened < len(logons_opened.index) and ses_closed < len(
-        logons_closed.index
-    ):
-        ses_start = (logons_opened.iloc[ses_opened]).name
-        ses_end = (logons_closed.iloc[ses_closed]).name
-        # If we can identify a user for the session add this to the details
-        if "User" in logons_opened.columns:
-            user = (logons_opened.iloc[ses_opened]).User
-        elif "Sudoer" in logons_opened.columns:
-            user = (logons_opened.iloc[ses_opened]).Sudoer
-        else:
-            user = "Unknown"
-        if ses_start > ses_close_time or ses_opened == 0:
-            pass
-        else:
-            ses_opened += 1
-            continue
-        if ses_end < ses_start:
-            ses_closed += 1
-            continue
-        # Produce dictionary of logon session components with descriptive string as key
-        logon_string = (
-            f"Logged on user: {user} Start time: {ses_start} End time: {ses_end}"
-        )
-        logon_sessions[logon_string] = {
-            "start": ses_start,
-            "end": ses_end,
-            "user": user,
-        }
-        ses_close_time = ses_end
-        ses_closed = ses_closed + 1
-        ses_opened = ses_opened + 1
-    return logon_sessions
-
-
-@export
-def cluster_syslog_logons_df(logon_events: pd.DataFrame) -> dict:
+def cluster_syslog_logons_df(logon_events: pd.DataFrame) -> pd.DataFrame:
     """
     Clusters logon sessions observed in syslog by start and end time based on PAM events.
 
@@ -263,7 +174,7 @@ def cluster_syslog_logons_df(logon_events: pd.DataFrame) -> dict:
 
     Returns
     -------
-    logon_sessions: dict
+    logon_sessions: pd.DataFrame
         A dictionary of logon sessions including start and end times and logged on user
 
     Raises
@@ -327,151 +238,15 @@ def cluster_syslog_logons_df(logon_events: pd.DataFrame) -> dict:
         ses_close_time = ses_end
         ses_closed = ses_closed + 1
         ses_opened = ses_opened + 1
-    logon_sessions_df = pd.DataFrame({"User": users, "Start": starts, "Ends": ends})
+    logon_sessions_df = pd.DataFrame({"User": users, "Start": starts, "End": ends})
     return logon_sessions_df
 
 
 @export
-def risky_cmd_line(
-    events: pd.DataFrame,
-    risky_stuff: str = os.path.join(
-        os.path.join(os.path.dirname(os.path.dirname(__file__)), _DETECTIONS_DEF_DIR),
-        "lx_cmd_line.json",
-    ),
-) -> dict:
-    """
-    Detect patterns of risky commands in syslog messages.
-
-    Risky patterns are defined in a json format file.
-
-    Parameters
-    ----------
-    events: pd.DataFrame
-        A DataFrame of all syslog events potentially containing risky command line activity
-    risky_stuff: str, optional
-        Path to json file containing patterns of risky activity to detect.
-        (Defaults to msticpy/resources/lx_cmd_line.json)
-
-    Returns
-    -------
-    risky actions: dict
-        A dictionary of commands that match a risky pattern
-
-    Raises
-    ------
-    DataError
-        This function currently only supports Syslog
-
-    """
-    if "SyslogMessage" not in events.columns:
-        raise DataError("This function currently only supports Syslog")
-
-    # Handle situations where command line activity has already been extracted to a 'Command' column
-    if "Command" in events.columns:
-        events["Command"].replace("", np.nan, inplace=True)
-        syslog_actions = (
-            events[["TimeGenerated", "Command"]]
-            .rename(columns={"Command": "Message"})
-            .dropna()
-            .set_index("TimeGenerated")
-            .to_dict()
-        )
-    else:
-        events["SyslogMessage"].replace("", np.nan, inplace=True)
-        syslog_actions = (
-            events[["TimeGenerated", "SyslogMessage"]]
-            .rename(columns={"SyslogMessage": "Message"})
-            .dropna()
-            .set_index("TimeGenerated")
-            .to_dict()
-        )
-    with open(risky_stuff) as json_file:
-        risky_data = json.load(json_file)
-
-    # Decode any Base64 encoded commands so we can match on them as well
-    b64_regex = re.compile(
-        "(?P<b64>(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{4})$)"
-    )
-    risky_actions = {}
-    risky_lines = risky_data["Syslog"]
-    for line in risky_lines:
-        for date, message in syslog_actions["Message"].items():
-            if b64_regex.match(message):
-                b64match = b64_regex.search(message)
-                b64string = unpack(input_string=b64match[1])
-                b64string = b64string[1]["decoded_string"].to_string()
-                if re.match(line, message):
-                    risky_actions.update({date: message})
-                else:
-                    pass
-            else:
-                if re.match(line, message):
-                    risky_actions.update({date: message})
-                else:
-                    pass
-    return risky_actions
-
-
-@export
-def sudo_actions_speed(
-    sudo_events: pd.DataFrame, time: int = 5, events: int = 5
-) -> list:
-    """
-    Detect patterns of sudo activity whose speed of execution may be suspicious.
-
-    Parameters
-    ----------
-    sudo_events: pd.DataFrame
-        A DataFrame of all sudo events to check.
-    time: int, optional
-        Time window in seconds in which to evaluate speed of execution against
-        (Defaults to 5)
-    events: int, optional
-        Number of syslog command execution events in which to evaluate speed of execution against
-        (Defaults to 5)
-
-    Returns
-    -------
-    risky suspicious_actions: list
-        A list of commands that match a risky pattern
-
-    Raises
-    ------
-    DataError
-        If data provided is not in the correct format
-
-    """
-    if "Command" not in sudo_events.columns:
-        raise DataError(
-            "DataFrame must contain column of commands run called 'Command'"
-        )
-
-    if isinstance(sudo_events["TimeGenerated"].iloc[0], dt.datetime) is False:
-        raise DataError("TimeGenerated is not a datetime format")
-
-    suspicious_actions = []
-    sudo_events["Command"].replace("", np.nan, inplace=True)
-    # Only focus on logs that contain comand line activity
-    sudo_actions = sudo_events.dropna(subset=["Command"]).reset_index()
-    df_len = len(sudo_actions.index) - (events + 1)
-    while df_len >= 0:
-        delta = (
-            sudo_actions["TimeGenerated"][(df_len + events)]
-            - sudo_actions["TimeGenerated"][df_len]
-        )
-        if delta < dt.timedelta(seconds=time):
-            suspicious_actions.append(
-                {df_len: [sudo_actions[df_len : (df_len + events)], delta]}
-            )
-        else:
-            pass
-        df_len = df_len - 1
-    return suspicious_actions
-
-
-@export
 def risky_sudo_sessions(
-    sudo_sessions: dict, risky_actions: dict = None, suspicious_actions: list = None
+    sudo_sessions: pd.DataFrame,
+    risky_actions: dict = None,
+    suspicious_actions: list = None,
 ) -> dict:
     """
     Detect if a sudo session occurs at the point of a suspicious event.
@@ -481,9 +256,9 @@ def risky_sudo_sessions(
     sudo_sessions: dict
         Dictionary of sudo sessions (as generated by cluster_syslog_logons)
     risky_actions: dict (Optional)
-        Dictionary of risky sudo commands (as generated by risky_actions)
+        Dictionary of risky sudo commands (as generated by cmd_line.risky_cmd_line)
     suspicious_actions: list (Optional)
-        List of risky sudo commands (as generated by risky_actions)
+        List of risky sudo commands (as generated by cmd_line.cmd_speed)
 
     Returns
     -------
@@ -491,6 +266,8 @@ def risky_sudo_sessions(
         A dictionary of sudo sessions with flags denoting risk
 
     """
+    sessions = sudo_sessions[["User", "Start", "End"]].to_dict("index")
+
     if risky_actions is None and suspicious_actions is None:
         raise DataError(
             "At least one of risky_actions or suspicious_actions must be supplied"
@@ -501,18 +278,18 @@ def risky_sudo_sessions(
     # identify sessions which these actions occur in
     if risky_actions is not None and suspicious_actions is None:
         risky_sessions = _find_risky_sudo_session(
-            risky_actions=risky_actions, sudo_sessions=sudo_sessions
+            risky_actions=risky_actions, sudo_sessions=sessions
         )
     elif suspicious_actions is not None and risky_actions is None:
         risky_sessions = _find_suspicious_sudo_session(
-            suspicious_actions=suspicious_actions, sudo_sessions=sudo_sessions
+            suspicious_actions=suspicious_actions, sudo_sessions=sessions
         )
     else:
         risky_act_sessions = _find_risky_sudo_session(
-            risky_actions=risky_actions, sudo_sessions=sudo_sessions
+            risky_actions=risky_actions, sudo_sessions=sessions
         )
         susp_sessions = _find_suspicious_sudo_session(
-            suspicious_actions=suspicious_actions, sudo_sessions=sudo_sessions
+            suspicious_actions=suspicious_actions, sudo_sessions=sessions
         )
         risky_sessions = {**risky_act_sessions, **susp_sessions}
     return risky_sessions
@@ -533,9 +310,9 @@ def _find_risky_sudo_session(risky_actions: dict, sudo_sessions: dict):
     for key, value in risky_actions.items():
         for sess_key, sess_val in sudo_sessions.items():
             if (
-                _normalize_to_utc(sess_val["start"])
+                _normalize_to_utc(sess_val["Start"])
                 <= _normalize_to_utc(key)
-                <= _normalize_to_utc(sess_val["end"])
+                <= _normalize_to_utc(sess_val["End"])
             ):
                 risky_sessions.update({sess_key: value})
     return risky_sessions
@@ -548,9 +325,9 @@ def _find_suspicious_sudo_session(suspicious_actions: list, sudo_sessions: dict)
         for value in event.values():
             for sess_key, sess_val in sudo_sessions.items():
                 if (
-                    _normalize_to_utc(sess_val["start"])
+                    _normalize_to_utc(sess_val["Start"])
                     <= _normalize_to_utc(value[0]["TimeGenerated"].iloc[1])
-                    <= _normalize_to_utc(sess_val["end"])
+                    <= _normalize_to_utc(sess_val["End"])
                 ):
                     risky_sessions.update({sess_key: "Suspicious event pattern"})
     return risky_sessions
--- tests/test_linuxsyslog.py
+++ tests/test_linuxsyslog.py
@@ -8,6 +8,7 @@ import os
 import pandas as pd
 from ..msticpy.nbtools.entityschema import Host
 from ..msticpy.sectools import syslog_utils as ls
+from ..msticpy.sectools import cmd_line as cl
 
 _test_data_folders = [
     d for d, _, _ in os.walk(os.getcwd()) if d.endswith("/tests/testdata")
@@ -16,13 +17,6 @@ if len(_test_data_folders) == 1:
     _TEST_DATA = _test_data_folders[0]
 else:
     _TEST_DATA = "./tests/testdata"
-
-
-def test_cluster_syslog_logons():
-    input_file = os.path.join(_TEST_DATA, "linux_logons.csv")
-    input_df = pd.read_csv(input_file, parse_dates=["TimeGenerated"])
-    output = ls.cluster_syslog_logons(input_df)
-    assert len(output) >= 1
 
 
 def test_cluster_syslog_logons_df():
@@ -44,51 +38,25 @@ def test_host_data():
     assert host_record.OSType == "Linux"
 
 
-def test_sudo_risky_actions_default():
-    input_file = os.path.join(_TEST_DATA, "sudo_data.csv")
-    input_df = pd.read_csv(input_file)
-    output = ls.risky_cmd_line(input_df)
-    assert len(output) >= 1
-    assert type(output) == dict
-    assert output["2019-07-05T18:19:52.873Z"] == "/bin/bash"
-
-
-def test_speed():
-    input_file = os.path.join(_TEST_DATA, "sudo_data_speed.csv")
-    input_df = pd.read_csv(input_file, parse_dates=["TimeGenerated"])
-    output = ls.sudo_actions_speed(input_df)
-    assert len(output) >= 1
-    assert type(output[0]) == dict
-
-
 def test_cluster_sudo_sessions():
     input_file = os.path.join(_TEST_DATA, "sudo_events.csv")
     input_df = pd.read_csv(input_file, parse_dates=["TimeGenerated"])
-    output = ls.cluster_syslog_logons(input_df)
-    assert len(output) >= 1
+    output = ls.cluster_syslog_logons_df(input_df)
+    assert len(output.index) >= 1
 
 
 def test_risky_sudo_sessions():
     input_file = os.path.join(_TEST_DATA, "sudo_session_test.csv")
     sudo_events = pd.read_csv(input_file, parse_dates=["TimeGenerated"])
-    risky_actions = ls.risky_cmd_line(events=sudo_events)
-    suspicious_events = ls.sudo_actions_speed(
-        sudo_events=sudo_events, time=60, events=2
+    risky_actions = cl.risky_cmd_line(events=sudo_events, log_type="Syslog")
+    suspicious_events = cl.cmd_speed(
+        cmd_events=sudo_events, cmd_field="Command", time=60, events=2
     )
-    sudo_sessions = ls.cluster_syslog_logons(logon_events=sudo_events)
+    sudo_sessions = ls.cluster_syslog_logons_df(logon_events=sudo_events)
     output = ls.risky_sudo_sessions(
         risky_actions=risky_actions,
         suspicious_actions=suspicious_events,
         sudo_sessions=sudo_sessions,
     )
     assert len(output) == 2
-    assert type(output) == dict
-
-
-def test_syslog_risky_actions():
-    input_file = os.path.join(_TEST_DATA, "syslog_data.csv")
-    input_df = pd.read_csv(input_file)
-    risky_stuff = os.path.join(_TEST_DATA, "risky_stuff_custom.json")
-    output = ls.risky_cmd_line(input_df, risky_stuff)
-    assert len(output) >= 1
     assert type(output) == dict
