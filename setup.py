b2b2520c11c5796af0fb047be7b2a3ba9561385e
Pete Bryan <pebryan@microsoft.com>
2019-12-09 09:55:39 -0800

    updated setup with bokeh version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 
 INSTALL_REQUIRES = [
     "attrs>=18.2.0",
-    "bokeh>=1.0.2",
+    "bokeh>=1.4.0",
     "cryptography>=2.8",
     "deprecated>=1.2.4",
     "dnspython>=1.16.0",
