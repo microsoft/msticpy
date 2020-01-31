2a83ce686435a3994b61eaf7865d5d88a48b81be
Ian Hellen <ianhelle@microsoft.com>
2020-01-31 13:22:07 -0800

    Fixing some tests an incorrect parameters used in notebooks
    Fixing AttributeError for pandas datetime value (seeming to result from update to Pandas 1.0)
    Changed tld_index and ssl_bl attributes to properties that auto-load on first use (prevents remote http request if data on class instantiation)
    Change environment variable that controls test skipping to something more generic MSTICPY_TEST_NOSKIP

--- tests/test_timeline.py
+++ tests/test_timeline.py
@@ -20,7 +20,7 @@ class TestTimeline(unittest.TestCase):
     """Unit test class."""
 
     @pytest.mark.skipif(
-        not os.environ.get("MSTICPY_TEST_NOTEBOOKS"), reason="Skipped for local tests."
+        not os.environ.get("MSTICPY_TEST_NOSKIP"), reason="Skipped for local tests."
     )
     def test_timeline_controls(self):
         nb_path = Path(_NB_FOLDER).joinpath(_NB_NAME)
