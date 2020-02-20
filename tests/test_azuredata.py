c11043873f19decce56aa23d3ebcb9be4385806e
Pete Bryan <peter.bryan@microsoft.com>
2020-02-20 11:10:02 -0800

    pipeline test

--- tests/test_azuredata.py
+++ tests/test_azuredata.py
@@ -4,16 +4,12 @@
 # license information.
 # --------------------------------------------------------------------------
 import os
-import pytest
-
-import pandas as pd
-
 from unittest.mock import patch
+
+from pytest import raises
 
 from ..msticpy.data.azure_data import AzureData
 from ..msticpy.nbtools.utility import MsticpyException
-from azure.mgmt.subscription import SubscriptionClient
-from azure.common.credentials import ServicePrincipalCredentials
 
 _test_data_folders = [
     d for d, _, _ in os.walk(os.getcwd()) if d.endswith("/tests/testdata")
@@ -30,13 +26,13 @@ def test_azure_init():
 
 
 def test_azure_connect_exp():
-    with pytest.raises(MsticpyException):
+    with raises(MsticpyException):
         az = AzureData()
         az.connect()
 
 
-@patch("msticpy.msticpy.data.azure_data.SubscriptionClient")
-@patch("msticpy.msticpy.data.azure_data.ServicePrincipalCredentials")
+@patch("msticpy.data.azure_data.SubscriptionClient")
+@patch("msticpy.data.azure_data.ServicePrincipalCredentials")
 def test_azure_connect(mock_sub_client, mock_creds):
     mock_sub_client.return_value = "Client"
     mock_creds.return_value = "Creds"
