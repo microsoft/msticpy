17eb5936a94368f5231585e9d7df5d717354c289
Ashwin Patil <ashwin-patil@users.noreply.github.com>
2020-03-18 14:40:13 -1000

    Timeseries - Bokeh with KQL (#49)
    * changes to range tool and timeseries module
    
    * column changes and inline code comments
    
    * time series query fixes
    
    * docstring and cosmetic changes
    
    * Added demo data and notebook for TimeSeries
    
    * added test case for timeseries notebooks
    
    * black formatting notebook and anomaly display
    
    * renaming alerts with anomalies in kql queries
    
    * Sphinx rst doc for TimeSeries demo notebook
    
    * moved workspace authentication code to markdown
    
    * fix build warnings
    
    * typo in pylint comments
    
    * pylint warnings and docstring changes
    
    * PR review changes and typo in docstrings
    
    * PR review changes, fixes
    
    Co-authored-by: Ian Hellen <ianhelle@microsoft.com>

---
+++ tests/test_timeseries.py
@@ -0,0 +1,41 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+import os
+import unittest
+from pathlib import Path
+
+import nbformat
+import notebook
+import pytest
+from nbconvert.preprocessors import CellExecutionError, ExecutePreprocessor
+
+_NB_FOLDER = "docs/notebooks"
+_NB_NAME = "TimeSeriesAnomaliesVisualization.ipynb"
+
+
+class TestTimeSeries(unittest.TestCase):
+    """Unit test class."""
+
+    @pytest.mark.skipif(
+        not os.environ.get("MSTICPY_TEST_NOSKIP"), reason="Skipped for local tests."
+    )
+    def test_timeseries_controls(self):
+        nb_path = Path(_NB_FOLDER).joinpath(_NB_NAME)
+        abs_path = Path(_NB_FOLDER).absolute()
+        with open(nb_path) as f:
+            nb = nbformat.read(f, as_version=4)
+        ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+
+        try:
+            ep.preprocess(nb, {"metadata": {"path": abs_path}})
+        except CellExecutionError:
+            nb_err = str(nb_path).replace(".ipynb", "-err.ipynb")
+            msg = f"Error executing the notebook '{nb_path}'.\n"
+            msg += f"See notebook '{nb_err}' for the traceback."
+            print(msg)
+            with open(nb_err, mode="w", encoding="utf-8") as f:
+                nbformat.write(nb, f)
+            raise
