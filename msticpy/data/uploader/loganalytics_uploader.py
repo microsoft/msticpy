4a54ea03145d788f2edb1705754dae905832a31e
petebryan <pebryan@microsoft.com>
2020-07-20 18:43:55 -0700

    progress tracking and error handling

--- msticpy/data/uploader/loganalytics_uploader.py
+++ msticpy/data/uploader/loganalytics_uploader.py
@@ -10,12 +10,17 @@ import datetime
 import hashlib
 import hmac
 import base64
+import sys
+from tqdm import tqdm
 from pathlib import Path
 import pandas as pd
+
+from ...common.exceptions import MsticpyConnectionError
 
 from .uploader_base import UploaderBase
 from ..._version import VERSION
 
+# Credits
 # https://docs.microsoft.com/en-us/azure/azure-monitor/platform/data-collector-api#python-3-sample
 # https://github.com/Cyb3rWard0g/azure-loganalytics-api-clients/blob/master/python/ala-python-data-producer.py
 
@@ -45,45 +50,56 @@ class LAUploader(UploaderBase):
         authorization = f"SharedKey {self.workspace}:{encoded_hash}"
         return authorization
 
-    def _post_data(self, body, LOG_TYPE):
+    def _post_data(self, body, table_name):
         resource = '/api/logs'
         content_type = 'application/json'
         rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
         content_length = len(body)
-        signature = self._build_signature(self.workspace, self.workspace_secret, rfc1123date, content_length, 'POST', content_type, resource)
+        signature = self._build_signature(rfc1123date, content_length, 'POST', content_type, resource)
         uri = 'https://' + self.workspace + self.ops_loc + resource + '?api-version=2016-04-01'
 
         headers = {
             'content-type': content_type,
             'Authorization': signature,
-            'Log-Type': LOG_TYPE,
+            'Log-Type': table_name,
             'x-ms-date': rfc1123date
         }
         response = requests.post(uri,data=body, headers=headers)
-        print(f'Response code: {response.status_code}')
+        print(response.status_code)
+        if (response.status_code < 200 or response.status_code > 299):
+            raise MsticpyConnectionError("LogAnalytics data upload failed, check Workspace ID and key")
 
     def upload_df(self, data:pd.DataFrame, table_name:str):
         events = []
-        # Make sure every column is a string
         for row in data.iterrows():
-            events.append(row[1].to_dict())
-        body = json.dumps(events)
-        self._post_data(body, table_name)
+            events.append(row[1].astype(str).to_dict())
+            # Due to 30MB limit if data is larger than 25Mb upload that chunk then continue
+            if sys.getsizeof(json.dumps(events)) > 26214400:
+                body = json.dumps(events)
+                self._post_data(body, table_name)
+                events = []
+
+        if events:
+            body = json.dumps(events)
+            self._post_data(body, table_name)
     
-    # Add in Roberto's data chunking
-    def upload_file(self, file_path:str, table_name:str, delim:str = ',',):
+
+    def file_upload(self, file_path:str, table_name:str, delim:str = ',',):
         file = Path(path)
         data = pd.read_csv(path, delim=delim)
         self.upload_df(data, table_name)
 
-    def upload_folder(self, folder_path:str, table_name:str = None, delim:str = ','):
+    def folder_upload(self, folder_path:str, table_name:str = None, delim:str = ','):
         if delim != ",":
             ext = "*"
         else:
             ext = "*.csv"
         input_files = Path(in_dir).glob(ext)
+        progress = tqdm(total=len(list(input_files)), desc='Files', position=0)
         for file in list(input_files):
             data = pd.read_csv(file, delimiter=delim)
             if not table_name:
                 table_name = str(file).split("\\")[-1].split(".")[0]
             self.upload_df(data, table_name)
+            progress.update(1)
+        progress.close()
