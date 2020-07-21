6c98db0bddcf31ea8312180166a6e4fb21322da1
petebryan <pebryan@microsoft.com>
2020-07-20 18:43:55 -0700

    progress tracking and error handling

--- msticpy/data/uploader/splunk_uploader.py
+++ msticpy/data/uploader/splunk_uploader.py
@@ -10,6 +10,7 @@ import datetime
 import hashlib
 import hmac
 import base64
+from tqdm import tqdm
 from pathlib import Path
 import pandas as pd
 import splunklib.client
@@ -18,14 +19,10 @@ import splunklib.results
 from .uploader_base import UploaderBase
 from ..._version import VERSION
 
-# https://docs.microsoft.com/en-us/azure/azure-monitor/platform/data-collector-api#python-3-sample
-# https://github.com/Cyb3rWard0g/azure-loganalytics-api-clients/blob/master/python/ala-python-data-producer.py
-
-
 __version__ = VERSION
 __author__ = "Pete Bryan"
 
-class LAUploader(UploaderBase):
+class SplunkUploader(UploaderBase):
     """Uploader class for LogAnalytics."""
 
     def __init__(self, username:str, workspace:str, workspace_secret:str, **kwargs):
@@ -40,26 +37,31 @@ class LAUploader(UploaderBase):
     def connect(self):
         # Handle auth failure
         self.service = splunklib.client.connect(host=self.workspace, username=self.user, password=self.workspace_secret)
+        print('Connected')
         self.connected = True
 
     def _post_data(self, data:pd.DataFrame, index_name:str, table_name:str, host:str = "Upload"):
-        index = load_index(index_name)
-        for row in data.iterows():
+        index = self._load_index(index_name)
+        progress = tqdm(total=len(data.index), desc='Rows', position=0)
+        for row in data.iterrows():
             data = row[1].to_csv()
             index.submit(data, sourcetype=table_name, host=host)
-    
+            progress.update(1)
+        progress.close() 
+        print('Upload complete')
+
     # Allow passing through host value
     def upload_df(self, data:pd.DataFrame, table_name:str, index_name:str):
         self._post_data(data, table_name, index_name)
     
     # Allow passing through host value
-    def upload_file(self, file_path:str, table_name:str, delim:str = ','):
+    def file_upload(self, file_path:str, table_name:str, delim:str = ','):
         file = Path(path)
         data = pd.read_csv(path, delim=delim)
         self._post_data(data, table_name, index_name)
     
     # Allow passing through host value
-    def upload_folder(self, folder_path:str, table_name:str = None, delim:str = ',', index_name):
+    def folder_upload(self, folder_path:str, index_name:str, table_name:str = None, delim:str = ',', ):
         if delim != ",":
             ext = "*"
         else:
@@ -71,15 +73,15 @@ class LAUploader(UploaderBase):
                 table_name = str(file).split("\\")[-1].split(".")[0]
             self._post_data(data, table_name, index_name)
 
-    def check_index(self, index_name:str):
-        service_list = [item.name for item in service.indexes]
+    def _check_index(self, index_name:str):
+        service_list = [item.name for item in self.service.indexes]
         if index_name in service_list:
             return True
         else: 
             return False
 
-    def load_index(self, index_name):
-        if check_index(index_name) is False:
+    def _load_index(self, index_name):
+        if self._check_index(index_name) is False:
             return self.service.indexes.create(index_name)
         else:
             return self.service.indexes[index_name]
