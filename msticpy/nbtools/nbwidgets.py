77b564c3dc514e63e3ebac563d4040ca4e4c47ad
Pete Bryan <pebryan@microsoft.com>
2021-11-19 15:03:04 -0800

    formatting

--- msticpy/nbtools/nbwidgets.py
+++ msticpy/nbtools/nbwidgets.py
@@ -1112,7 +1112,9 @@ class SelectItem:
             raise ValueError("One of item_list or item_dict must be supplied.")
 
         # Check default value is actually present in our list of items
-        list_def_val = self.def_value if self.def_value in self._item_list else self.value
+        list_def_val = (
+            self.def_value if self.def_value in self._item_list else self.value
+        )
 
         self._wgt_select = widgets.Select(
             value=list_def_val,
