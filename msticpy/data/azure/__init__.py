524e44b6668b64f79f0f25392fdb62c78ee855b0
Pete Bryan <pebryan@microsoft.com>
2022-01-10 17:12:42 -0800

    Sentinel mixin creation and folder restructure

---
+++ msticpy/data/azure/__init__.py
@@ -0,0 +1,9 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""Data provider sub-package."""
+
+from .azure_data import AzureData
+from .sentinel_core import MicrosoftSentinel
