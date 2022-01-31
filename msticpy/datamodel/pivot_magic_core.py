4e5ad8bf0509bf616d36948456e6173011793023
Pete Bryan <pebryan@microsoft.com>
2022-01-31 15:25:46 -0800

    Added additional stub files

---
+++ msticpy/datamodel/pivot_magic_core.py
@@ -0,0 +1,22 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""Deprecated placeholder for pivot_magic_core.py."""
+import warnings
+from .._version import VERSION
+
+__version__ = VERSION
+__author__ = "Pete Bryan"
+
+
+# flake8: noqa: F403, F401
+# pylint: disable=wildcard-import, unused-wildcard-import, unused-import
+from .pivots.pivot_magic_core import *
+
+WARN_MSSG = (
+    "This module has moved to msticpy.datamodel.pivots.pivot_magic_core\n"
+    + "Please change your import to reflect this new location."
+)
+warnings.warn(WARN_MSSG, category=DeprecationWarning)
