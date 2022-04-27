355f8de41c7746466b3297984c87d28a0010fd5e
Ian Hellen <ianhelle@microsoft.com>
2022-04-27 13:40:39 -0700

    Forgot to add "r" prefix to strings in test_sentinel_search

--- tests/data/azure/test_sentinel_search.py
+++ tests/data/azure/test_sentinel_search.py
@@ -29,21 +29,21 @@ def sent_loader(mock_creds):
 @respx.mock
 def test_sent_search_create(sent_loader):
     """Test Sentinel Watchlist feature."""
-    respx.put(re.compile("https://management\.azure\.com/.*")).respond(202)
+    respx.put(re.compile(r"https://management\.azure\.com/.*")).respond(202)
     sent_loader.create_search(query="Syslog| take 10", search_name="testsearch")
 
 
 @respx.mock
 def test_sent_search_delete(sent_loader):
     """Test Sentinel Watchlist feature."""
-    respx.delete(re.compile("https://management\.azure\.com/.*")).respond(202)
+    respx.delete(re.compile(r"https://management\.azure\.com/.*")).respond(202)
     sent_loader.delete_search(search_name="testsearch")
 
 
 @respx.mock
 def test_sent_search_check_pending(sent_loader):
     """Test Sentinel Watchlist feature."""
-    respx.get(re.compile("https://management\.azure\.com/.*")).respond(
+    respx.get(re.compile(r"https://management\.azure\.com/.*")).respond(
         200, json={"properties": {"provisioningState": "InProgress"}}
     )
     assert not sent_loader.check_search_status(search_name="testsearch")
@@ -52,7 +52,7 @@ def test_sent_search_check_pending(sent_loader):
 @respx.mock
 def test_sent_search_check_complete(sent_loader):
     """Test Sentinel Watchlist feature."""
-    respx.get(re.compile("https://management\.azure\.com/.*")).respond(
+    respx.get(re.compile(r"https://management\.azure\.com/.*")).respond(
         200, json={"properties": {"provisioningState": "Succeeded"}}
     )
     assert sent_loader.check_search_status(search_name="testsearch")
