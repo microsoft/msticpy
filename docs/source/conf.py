2e8e64acdc1d5b297e74db2634ea0cc381a7eadc
Ian Hellen <ianhelle@microsoft.com>
2022-06-16 14:40:40 -0700

    Updating docs/requirements.txt
    Adding intersphinx to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,8 +62,8 @@ extensions = [
     "sphinx.ext.githubpages",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosectionlabel",
-    # "sphinx.ext.intersphinx",
-    # "seed_intersphinx_mapping",
+    "sphinx.ext.intersphinx",
+    "seed_intersphinx_mapping",
 ]
 
 autosectionlabel_prefix_document = True
@@ -85,7 +85,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
