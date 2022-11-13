478924647dd5113497eb897b6765903519bf1326
FlorianBracq <97248273+FlorianBracq@users.noreply.github.com>
2022-11-13 03:09:40 +0100

    Add support for context provider Service Now (#556)
    * Rename and reorganize files
    
    * Add new base classes
    
    * Set children classes to use new classes
    
    * Update TI classes & tests to rely on new structure
    
    * Adding context providers
    
    * Fix imports
    
    * Fix black formatting issues
    
    * Fix documentation
    
    * Fix typing inconsistency
    
    * Fix import error
    
    * Fix documentation linting
    
    * Fix import and param names
    
    * Align parameter names
    
    * * Remove references to LookupResult
    * Simplify underlying implementation
    * Always return dataframes, even when using lookup_ioc
    
    * Update TI Providers not to use LookupResult
    Relying on a Dict instead
    
    * Updating tests to align on new methods/parameters
    
    * Fix formatting issues
    
    * Fix conditional branching in inheritance
    and parameter type changes
    
    * Fix openpage typing error
    
    * Fix default severity value for IOC
    
    * Remove unused import
    
    * Remove references to LookupResult
    Only use original LookupStatus
    
    * s/*_QUERIES/_QUERIES/
    Use APILookupParam instead of IOCLookupParam
    
    * Rename HttpLookupProvider toHttpProvider
    Rename http_lookup.py to http_provider.py
    
    * Set parameters are Optional is default to None
    
    * Re-define result_to_df based on param being a df.
    
    * Set _QUERIES with the complete type
    
    * Update tests
    
    * Fix test
    
    * Only test browser if Severity is not unknown
    
    * Minor changes to lookup - main change is removing unneeded LookupResult classes.
    
    Generated API docs for new/changed modules
    
    * Remove reference to lookupResult
    
    * Rework calls between lookup_item and observable
    Rename obs_type to observable_type
    
    * Remove unecessary line
    
    * Adding ServiceNow class
    
    * Splitting TIProviders and "base providers" tests
    
    * Adding contextproviders key to msticpyconfig test files
    
    * Adding test for context providers
    
    * Removing unrequired lines
    
    * Adding customer hostname and IP parsers
    * IP Parser to support local IP addresses
    * hostname to support short names
    
    * Black formatting
    
    * Add noqa F401 for contextprovider's init.py
    
    * Fixing test_tiproviders.py - renames due to merge.
    
    Co-authored-by: Ian Hellen <ianhelle@microsoft.com>

--- msticpy/context/contextproviders/__init__.py
+++ msticpy/context/contextproviders/__init__.py
@@ -2,7 +2,10 @@
 from typing import Dict, Tuple
 
 from ..._version import VERSION
+from .servicenow import ServiceNow  # noqa:F401
 
 __version__ = VERSION
 
-CONTEXT_PROVIDERS: Dict[str, Tuple[str, str]] = {}
+CONTEXT_PROVIDERS: Dict[str, Tuple[str, str]] = {
+    "ServiceNow": ("servicenow", "ServiceNow"),
+}
