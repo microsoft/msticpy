bb3b4f8009fbf7323e6ce48c773f224645c174fc
Ian Hellen <ianhelle@microsoft.com>
2023-03-08 11:02:21 -0800

    Adding regular TI provider for pulsedive - pulsedive.py and __init__.py
    Refactored some code in original class to make it a bit cleaner.
    Added unit test in test_pulsedive.py
    Some minor fixes for MyPy and linting errors.
    Updated notebook for modified API and to describe standard Pulsedive TIProvider use.

--- msticpy/context/tiproviders/pulsedive.py
+++ msticpy/context/tiproviders/pulsedive.py
@@ -7,13 +7,21 @@
 """Pulsedive TI Provider."""
 
 from enum import Enum
-from typing import Set
+from time import sleep
+from typing import Dict, NamedTuple, Optional, Set
 
 import httpx
 import pandas as pd
 from pandas import json_normalize
 
+from msticpy.common.pkg_config import get_http_timeout
+from msticpy.common.provider_settings import get_provider_settings
+from msticpy.common.utility import mp_ua_header
+
 from ..._version import VERSION
+from ..http_provider import APILookupParams
+from .result_severity import LookupResult, ResultSeverity
+from .ti_http_provider import HttpTIProvider
 
 __version__ = VERSION
 __author__ = "Thomas Roccia | @fr0gger_"
@@ -25,7 +33,14 @@ _QUERY_OBJECTS_MAPPINGS = {
     "threat": {"threat": "observable"},
     "explore": {"q": "query"},
     "scan": {"scan": "observable"},
-    }
+}
+
+
+class PDQuery(NamedTuple):
+    """Query object for Pulsedive queries."""
+
+    query_type: str
+    data: str
 
 
 class PDEntityType(Enum):
@@ -39,11 +54,14 @@ class PDEntityType(Enum):
     THREAT: Queries information about a specific threat
     EXPLORE: Executes a general search across the Pulsedive data set
     SCAN: Submits an indicator of compromise (IOC) for scanning
+
     """
+
     INDICATOR = "indicator"
     THREAT = "threat"
     EXPLORE = "explore"
     SCAN = "scan"
+
 
 class PDlookup:
     """
@@ -52,10 +70,12 @@ class PDlookup:
     This class allows you to look up indicators of compromise (IOCs) and
     explore different types of data available in the Pulsedive API.
 
-    Attributes:
+    Attributes
+    ----------
     pd_key (str): An API key for the Pulsedive API.
 
-    Methods:
+    Methods
+    -------
     _make_pd_request(data): Makes a request to the Pulsedive API with the provided data.
     lookup_ioc(observable: str, pd_type: str) -> pd.DataFrame: Lookup an indicator of
     compromise in the Pulsedive API.
@@ -63,6 +83,7 @@ class PDlookup:
     available in the Pulsedive API.
     scan(observable: str, pd_type: str) -> pd.DataFrame: Submits an indicator of compromise
     to the Pulsedive API for scanning.
+
     """
 
     _SUPPORTED_PD_TYPES: Set[PDEntityType] = {
@@ -74,105 +95,209 @@ class PDlookup:
 
     def __init__(self, pd_key=None):
         """Init function to get the API key if necessary."""
-        self.pd_key = pd_key  # or_get_mb_api_key()
+        self.pd_key = pd_key or self._get_pd_api_key()
 
-    def _make_pd_request(self, data):
-        """
-        Makes a request to the PulseDive API with the provided data.
-
-        Parameters:
-        - data (dict): a dictionary containing the query parameters to send in the request
-
-        Returns:
-        - pd.DataFrame: a dataframe containing the results of the API call
-        """
-
-        for key, value in data.items():
-            if key == "q":
-                # if the key is "q", make a GET request to the explore endpoint
-                res = httpx.get(f'{_BASE_URL}explore.php?{key}={value}\
-                    &pretty=1&key={self.pd_key}', timeout=None)
-                if res.status_code == 200:
-                    jspd = res.json()
-                    return json_normalize(jspd['results'])
-
-            elif key == "scan":
-                # if the key is "scan", make a POST request to the analyze endpoint
-                data = {"value": value, "probe": 1, "pretty": 1, "key": self.pd_key}
-                headers = {"Content-Type": "application/x-www-form-urlencoded"}
-                res = httpx.post(f"{_BASE_URL}analyze.php?", data=data, headers=headers)
-                if res.status_code == 200:
-                    # if the initial request is successful, get the qid and
-                    # check the status until it is 'success'
-                    qid = res.json()['qid']
-                    res = httpx.get(f"{_BASE_URL}analyze.php?qid={qid}\
-                        &pretty=1&key={self.pd_key}")
-                    status = res.json()['status']
-                    print(status)
-                    while status == 'processing':
-                        result = httpx.get(f"{_BASE_URL}analyze.php?qid={qid}\
-                            &pretty=1&key={self.pd_key}")
-                        if next(iter(result.json())) != "error":
-                            break
-                    return json_normalize(result.json()["data"])
-            else:
-                # if the key is not "q" or "scan", make a GET request to the info endpoint
-                res = httpx.get(f"{_BASE_URL}info.php?{key}={value}\
-                    &pretty=1&key={self.pd_key}", timeout=None)
-                if res.status_code == 200:
-                    return json_normalize(res.json())
-
-                raise ValueError(f'API request failed with status \
-                    code {res.status_code} and message {res.text}')
-            return None
-
-
-    def lookup_ioc(self, observable: str, pd_type: str) -> pd.DataFrame:
+    def lookup_ioc(self, observable: str, pd_type: str = "indicator") -> pd.DataFrame:
         """
         Lookup an indicator of compromise (IOC) in the Pulsedive API.
-        :param observable: The IOC to lookup.
-        :param pd_type: The type of the IOC. Should be one of the PDEntityType Enum options.
-        :return: A Pandas DataFrame with the API's response.
+
+        Parameters
+        ----------
+        observable : str
+            The IOC to lookup.
+        pd_type : str
+            The lookup type to perform, default is "indicator".
+
+        Returns
+        -------
+        pd.DataFrame
+            A Pandas DataFrame with the API's response.
+
+        Notes
+        -----
+        In the case of `pd_type="explore"`, the `observable`
+        value should be a pulsedive query string.
+        See https://pulsedive.com/explore/
+
+        See Also
+        --------
+        PDlookup.lookup_threat
+        PDlookup.explore
+        PDlookup.scan
+
         """
-        if pd_type not in _QUERY_OBJECTS_MAPPINGS:
-            raise ValueError(f"Invalid pd_type : {pd_type}. \
-            Should be one of {_QUERY_OBJECTS_MAPPINGS.keys()}")
+        if pd_type == PDEntityType.THREAT.value:
+            return self.lookup_threat(observable=observable)
+        if pd_type == PDEntityType.EXPLORE.value:
+            return self.explore(query=observable)
+        if pd_type == PDEntityType.SCAN.value:
+            return self.scan(observable=observable)
+        return self._make_pd_request(
+            _build_query_string(observable, pd_type=PDEntityType.INDICATOR.value)
+        )
 
-        query_parameters = _build_query_string(observable, pd_type)
-        return self._make_pd_request(query_parameters)
-
-    def explore(self, query: str, pd_type: str) -> pd.DataFrame:
+    def lookup_threat(self, observable: str) -> pd.DataFrame:
         """
-        Perform a search query on the Pulsedive API.
-        :param query: The query to perform.
-        :param pd_type: The type of the query. Should be one of the PDEntityType Enum options.
-        :return: A Pandas DataFrame with the API's response.
+        Lookup a Threat name in the Pulsedive API.
+
+        Parameters
+        ----------
+        observable : str
+            The IOC to lookup.
+
+        Returns
+        -------
+        pd.DataFrame
+            A Pandas DataFrame with the API's response.
+
         """
-        if pd_type not in _QUERY_OBJECTS_MAPPINGS:
-            raise ValueError(f"Invalid pd_type : {pd_type}. \
-                Should be one of {_QUERY_OBJECTS_MAPPINGS.keys()}")
+        return self._make_pd_request(
+            _build_query_string(observable, pd_type=PDEntityType.THREAT.value)
+        )
 
-        query_parameters = _build_query_string(query, pd_type)
+    def explore(self, query: str) -> pd.DataFrame:
+        """Perform a search query on the Pulsedive API.
 
-        return self._make_pd_request(query_parameters)
+        Parameters
+        ----------
+        query : str
+            The query to perform.
 
-    def scan(self, observable: str, pd_type: str) -> pd.DataFrame:
+        Returns
+        -------
+        pd.DataFrame
+            A Pandas DataFrame with the API's response.
+
+        """
+        return self._make_pd_request(
+            _build_query_string(query, pd_type=PDEntityType.EXPLORE.value)
+        )
+
+    def scan(self, observable: str) -> pd.DataFrame:
         """
         Scan an observable in the Pulsedive API.
-        :param observable: The observable to scan.
-        :param pd_type: The type of the observable. Should be one of the PDEntityType Enum options.
-        :return: A Pandas DataFrame with the API's response.
+
+        Parameters
+        ----------
+        observable : str
+            The observable to scan.
+
+        Returns
+        -------
+        pd.DataFrame
+            A Pandas DataFrame with the API's response.
+
         """
+        return self._make_pd_request(
+            _build_query_string(observable, pd_type=PDEntityType.SCAN.value)
+        )
+
+    def _check_valid_type(self, pd_type):
         if pd_type not in _QUERY_OBJECTS_MAPPINGS:
-            raise ValueError(f"Invalid pd_type : {pd_type}. \
-                Should be one of {_QUERY_OBJECTS_MAPPINGS.keys()}")
+            raise ValueError(
+                f"Invalid pd_type: {pd_type}.",
+                f"Should be one of: {', '.join(_QUERY_OBJECTS_MAPPINGS.keys())}",
+            )
 
-        query_parameters = _build_query_string(observable, pd_type)
-        return self._make_pd_request(query_parameters)
+    def _make_pd_request(self, pd_query: PDQuery) -> pd.DataFrame:
+        """
+        Make a request to the PulseDive API with the provided data.
 
-def _build_query_string(data, pd_type):
-    """
-    Builds the query string for the API request based on the provided data and pd_type.
+        Parameters
+        ----------
+        pd_query : PDQuery
+            Request data
+
+        Returns
+        -------
+        pd.DataFrame
+            Dataframe containing the results of the API call
+
+        """
+        if pd_query.query_type == "scan":
+            # if the key is "scan", make a POST request to the analyze endpoint
+            data = {"value": pd_query.data, "probe": 1, **self._get_default_params()}
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded",
+                **mp_ua_header(),
+            }
+            resp = httpx.post(f"{_BASE_URL}analyze.php", data=data, headers=headers)
+            if resp.status_code != 200:
+                raise ValueError(
+                    f"API request failed with status code {resp.status_code}",
+                    f" and message {resp.text}",
+                )
+            return self._poll_for_results(resp)
+
+        if pd_query.query_type == "q":
+            # if the key is "q", make a GET request to the explore endpoint
+            query_url = f"{_BASE_URL}explore.php"
+        else:
+            query_url = f"{_BASE_URL}info.php"
+        params = {**self._get_default_params(), pd_query.query_type: pd_query.data}
+        resp = httpx.get(
+            query_url, params=params, headers=mp_ua_header(), timeout=get_http_timeout()
+        )
+        if resp.status_code != 200:
+            raise ValueError(
+                f"API request failed with status code {resp.status_code}",
+                f" and message {resp.text}",
+            )
+        json_resp = resp.json()
+        if pd_query.query_type == "q":
+            json_resp = json_resp["results"]
+        return json_normalize(json_resp)
+
+    def _get_default_params(self):
+        """Return default query parameters."""
+        return {"pretty": 1, "key": self.pd_key}
+
+    def _poll_for_results(self, resp):
+        """Poll API for results for this query ID."""
+        # if the initial request is successful, get the qid and
+        # check the status until it is 'success'
+        qid = resp.json()["qid"]
+        params = {"qid": qid, **self._get_default_params()}
+        headers = mp_ua_header()
+        timeout = get_http_timeout()
+        url = f"{_BASE_URL}analyze.php"
+        resp = httpx.get(
+            url,
+            params=params,
+            headers=headers,
+            timeout=timeout,
+        )
+        status = resp.json()["status"]
+        while status == "processing":
+            sleep(0.5)  # wait a little to avoid hammering UI
+            resp = httpx.get(url, params=params, headers=headers, timeout=timeout)
+            status = resp.json()["status"]
+
+        return json_normalize(resp.json()["data"])
+
+    def _get_pd_api_key(self) -> Optional[str]:
+        """Return Pulsedive api key from configuration."""
+        pd_settings = get_provider_settings("TIProviders").get("Pulsedive")
+        if pd_settings and "AuthKey" in pd_settings.args:
+            return pd_settings.args["AuthKey"]
+        return None
+
+
+def _build_query_string(data, pd_type) -> PDQuery:
+    """Build query string for the API request based on the provided data and pd_type.
+
+    Parameters
+    ----------
+    data : Any
+        Data to be mapped to query_item
+
+    pd_type : str
+
+    Returns
+    -------
+    Dict[str, Any]
+        Query items in dictionary
+
     """
     query_items = _QUERY_OBJECTS_MAPPINGS[pd_type]
 
@@ -180,8 +305,71 @@ def _build_query_string(data, pd_type):
     if not data:
         raise ValueError("data is empty.")
 
-    # assign the provided data to the corresponding key in the query_items dictionary
-    query_items = {list(query_items.keys())[0]: data}
+    return PDQuery(next(iter(query_items.keys())), data)
 
-    # return the final query_items dictionary
-    return query_items
+
+# -------------------------------
+# MSTICPY Standard TI Provider
+
+_QUERY_DEF = APILookupParams(
+    path="info.php",
+    params={"indicator": "{observable}", "pretty": 1, "key": "{API_KEY}"},
+)
+
+
+class Pulsedive(HttpTIProvider):
+    """Pulsedive TI Lookup."""
+
+    _BASE_URL = _BASE_URL
+
+    _QUERIES = {
+        ioc_type: _QUERY_DEF for ioc_type in ("ipv4", "ipv6", "dns", "hostname", "url")
+    }
+
+    _REQUIRED_PARAMS = ["API_KEY"]
+    _RISK_MAP = {
+        "high": ResultSeverity.high,
+        "medium": ResultSeverity.warning,
+        "low": ResultSeverity.information,
+        "none": ResultSeverity.unknown,
+    }
+
+    def __init__(self, **kwargs):
+        """Set OTX specific settings."""
+        super().__init__(**kwargs)
+        self.require_url_encoding = True
+
+    def parse_results(self, response: Dict) -> LookupResult:
+        """
+        Return the details of the response.
+
+        Parameters
+        ----------
+        response : Dict
+            The returned data response
+
+        Returns
+        -------
+        Tuple[bool, ResultSeverity, Any]
+            bool = positive or negative hit
+            ResultSeverity = enumeration of severity
+            Object with match details
+
+        """
+        if self._failed_response(response) or not isinstance(
+            response["RawResult"], dict
+        ):
+            return LookupResult(
+                False, ResultSeverity.information, {"status": "Not found."}
+            )
+        severity = self._RISK_MAP[response.get("risk", "none")]
+
+        details = {
+            "indicator_id": response.get("iid"),
+            "threats": response.get("threats", []),
+            "last_seen": response.get("stamp_seen", ""),
+            "risk_factors": response.get("riskfactors", {}),
+            "comments": response.get("comments", {}),
+        }
+
+        return LookupResult(True, severity, details)
