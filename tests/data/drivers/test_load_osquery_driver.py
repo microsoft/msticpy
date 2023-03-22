2c8ec0ebb3af10bf6d2f5464ed0fed30e818d84b
Ian Hellen <ianhelle@microsoft.com>
2023-03-22 11:24:31 -0700

    Missed the updated unit tests

--- tests/data/drivers/test_load_osquery_driver.py
+++ tests/data/drivers/test_load_osquery_driver.py
@@ -8,6 +8,7 @@ import pandas as pd
 import pytest
 import pytest_check as check
 
+from msticpy.data.core.data_providers import QueryProvider
 from msticpy.data.drivers.local_osquery_driver import OSQueryLogDriver
 
 from ...unit_test_lib import get_test_data_path
@@ -18,32 +19,45 @@ __author__ = "Ian Hellen"
 
 
 # change this for actual data
-EXPECTED_TABLES = 5
+
+_OSQUERY_LOG_PATH = "osquery"
+_EXPECTED_TABLES = 21
+_LOG_TOTAL_LINES = 309
+_EXPECTED_KEYS = [
+    "pack_osquery-snapshots-pack_platform_info",
+    "pack_osquery-snapshots-pack_python_packages",
+    "pack_osquery-snapshots-pack_dns_resolvers",
+]
 
 
-@pytest.mark.skip
 def test_read_log_file():
     """Test reading OSQuery logs."""
-    os_query_path = str(get_test_data_path().joinpath("os_query_data.log"))
+    os_query_path = str(get_test_data_path().joinpath(_OSQUERY_LOG_PATH))
     osq_driver = OSQueryLogDriver(data_paths=[os_query_path])
 
     osq_driver.connect()
-    check.equal(len(osq_driver._data_cache), EXPECTED_TABLES)
+    check.equal(len(osq_driver._data_cache), _EXPECTED_TABLES)
     for df in osq_driver._data_cache.values():
         check.is_instance(df, pd.DataFrame)
+    check.equal(
+        sum(len(df) for df in osq_driver._data_cache.values()), _LOG_TOTAL_LINES
+    )
+    for event_type in _EXPECTED_KEYS:
+        check.is_in(event_type, osq_driver._data_cache)
 
     check.is_instance(osq_driver.schema, dict)
-    check.equal(len(os_query_path.schema), EXPECTED_TABLES)
+    check.equal(len(osq_driver.schema), _EXPECTED_TABLES)
     for col_dict in osq_driver.schema.values():
-        check.is_greater(len(col_dict), 5)
+        check.greater(len(col_dict), 5)
         check.is_in("datetime64[ns, UTC]", col_dict.values())
+    for event_type in _EXPECTED_KEYS:
+        check.is_in(event_type, osq_driver.schema)
 
 
-@pytest.mark.skip
 def test_cache_file(tmp_path):
     """Test reading and writing cache file."""
-    os_query_path = str(get_test_data_path().joinpath("os_query_data.log"))
-    cache_path = tmp_path.join_path("osq_cache.pkl")
+    os_query_path = str(get_test_data_path().joinpath(_OSQUERY_LOG_PATH))
+    cache_path = tmp_path.joinpath("osq_cache.pkl")
     check.is_false(cache_path.is_file())
     osq_driver = OSQueryLogDriver(data_paths=[os_query_path], cache_file=cache_path)
     osq_driver.connect()
@@ -56,10 +70,23 @@ def test_cache_file(tmp_path):
     )
     osq_driver_cached.connect()
 
-    check.equal(len(osq_driver_cached._data_cache), EXPECTED_TABLES)
+    check.equal(len(osq_driver_cached._data_cache), _EXPECTED_TABLES)
     for df in osq_driver_cached._data_cache.values():
         check.is_instance(df, pd.DataFrame)
 
     for name, df in osq_driver._data_cache.items():
         check.is_in(name, osq_driver_cached._data_cache)
         check.equal(df.shape, osq_driver_cached._data_cache[name].shape)
+
+
+def test_query():
+    """Test loading and querying osquery data."""
+    os_query_path = str(get_test_data_path().joinpath(_OSQUERY_LOG_PATH))
+    qry_prov = QueryProvider("OSQueryLogs", data_paths=[os_query_path])
+    qry_prov.connect()
+    check.equal(len(qry_prov.osquery), 19)
+    check.equal(len(qry_prov.osquery.dns_resolvers()), 2)
+    check.equal(len(qry_prov.osquery.platform_info()), 1)
+    check.equal(len(qry_prov.osquery.python_packages()), 40)
+    check.equal(len(qry_prov.osquery.processes()), 21)
+    check.equal(len(qry_prov.osquery.shell_history()), 15)
