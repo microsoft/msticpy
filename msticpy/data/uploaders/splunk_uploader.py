a4627c6f5d453fb6d4971c7d26b60bc15d0e1786
hackeT <40039738+Tatsuya-hasegawa@users.noreply.github.com>
2024-04-12 17:10:59 +0900

    add jwt auth token expire check and modify some messages when connecting Splunk

--- msticpy/data/uploaders/splunk_uploader.py
+++ msticpy/data/uploaders/splunk_uploader.py
@@ -4,6 +4,7 @@
 # license information.
 # --------------------------------------------------------------------------
 """Splunk Uploader class."""
+import logging
 from pathlib import Path
 from typing import Any, Optional
 
@@ -17,19 +18,22 @@ from ..drivers.splunk_driver import SplunkDriver
 from .uploader_base import UploaderBase
 
 __version__ = VERSION
-__author__ = "Pete Bryan"
+__author__ = "Pete Bryan, Tatsuya Hasegawa"
+
+logger = logging.getLogger(__name__)
 
 
 class SplunkUploader(UploaderBase):
     """Uploader class for Splunk."""
 
-    def __init__(self, username: str, host: str, password: str, **kwargs):
+    def __init__(self, **kwargs):
         """Initialize a Splunk Uploader instance."""
         super().__init__()
         self._kwargs = kwargs
-        self.workspace = host
-        self.workspace_secret = password
-        self.user = username
+        self.host = kwargs.get("host")
+        self.password = kwargs.get("password")
+        self.username = kwargs.get("username")
+        self.bearer_token = kwargs.get("bearer_token")
         self.driver = SplunkDriver()
         self.port = kwargs.get("port", 8089)
         self._debug = kwargs.get("debug", False)
@@ -40,12 +44,27 @@ class SplunkUploader(UploaderBase):
 
     def connect(self):
         """Connect to Splunk host."""
-        self.driver.connect(
-            host=self.workspace,
-            username=self.user,
-            password=self.workspace_secret,
-            port=self.port,
-        )
+        if self._kwargs:
+            if self.username:
+                self.driver.connect(
+                    host=self.host,
+                    username=self.username,
+                    password=self.password,
+                    port=self.port,
+                )
+            elif self.bearer_token:
+                self.driver.connect(
+                    host=self.host,
+                    bearer_token=self.bearer_token,
+                    port=self.port,
+                )
+            else:
+                logger.warning("Both 'username' and 'bearer_token' are missing.")
+                logger.info("Either param is necessary.")
+                self.driver.connect()
+        else:
+            self.driver.connect()
+
         self.connected = True
 
     def _post_data(
@@ -81,11 +100,11 @@ class SplunkUploader(UploaderBase):
         index = self._load_index(index_name, kwargs.get("create_index", False))
         progress = tqdm(total=len(data.index), desc="Rows", position=0)
         for row in data.iterrows():
-            data = row[1].to_csv()
+            data = row[1].to_csv()  # type: ignore
             try:
-                data.encode(encoding="latin-1")
+                data.encode(encoding="latin-1")  # type: ignore
             except UnicodeEncodeError:
-                data = data.encode(encoding="utf-8")
+                data = data.encode(encoding="utf-8")  # type: ignore
             index.submit(data, sourcetype=table_name, host=host)
             progress.update(1)
         progress.close()
