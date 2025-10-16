2c92036adca531e69967f61e8de666a36b8af70e
ianhelle <ianhelle@microsoft.com>
2025-10-16 11:42:55 -0700

    Python 3.13 compatibility - Updating pyautogen dependencies for Python 3.13

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ EXTRAS = {
     "sql2kql": ["mo-sql-parsing>=8, <9.0.0"],
     "riskiq": ["passivetotal>=2.5.3", "requests>=2.31.0"],
     "panel": [],  # now in core install
-    "aiagents": ["autogen-agentchat[retrievechat]~=0.2.0"],
+    "aiagents": ["pyautogen[retrievechat]>=0.2.38"],
 }
 extras_all = [
     extra for name, extras in EXTRAS.items() for extra in extras if name != "dev"
