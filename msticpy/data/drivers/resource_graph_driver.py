4d5b9f364046f509579394dd62d5b6e8b19d8a61
ianhelle <ianhelle@microsoft.com>
2025-12-08 23:29:34 -0800

    More mypy errors from build

--- msticpy/data/drivers/resource_graph_driver.py
+++ msticpy/data/drivers/resource_graph_driver.py
@@ -164,7 +164,7 @@ class ResourceGraphDriver(DriverBase):
 
         request_options = QueryRequestOptions(
             top=top,
-            result_format=ResultFormat.object_array,
+            result_format=ResultFormat.object_array.value,  # type: ignore[attr-defined]
         )
 
         request = QueryRequest(
